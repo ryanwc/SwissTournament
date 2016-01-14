91ce9e9258d9db5ac3ff6497f83da80d1fa1700f
Ryan Connor <ryanwc13@gmail.com>
2016-01-14 17:40:41 +0700

    testScript now reflects new schema

--- testScript.sql
+++ testScript.sql
@@ -21,29 +21,41 @@ insert into Registration (PlayerID, TournamentID) values (3, 2);
 insert into Registration (PlayerID, TournamentID) values (4, 2);
 
 -- report badmitton matches
-insert into Match (Winner, WinnerPoints, Loser, LoserPoints, IsATie)
-	values (1, 2, 2, 0, False);
-insert into Match (Winner, WinnerPoints, Loser, LoserPoints, IsATie)
-	values (3, 1, 4, 0, False);
-insert into Match (Winner, WinnerPoints, Loser, LoserPoints, IsATie)
-	values (1, 3, 3, 2, False);
-insert into Match (Winner, WinnerPoints, Loser, LoserPoints, IsATie)
-	values (2, 1, 4, 0, False);
-insert into Match (Winner, WinnerPoints, Loser, LoserPoints, IsATie)
-	values (1, 3, 4, 1, False);
-insert into Match (Winner, WinnerPoints, Loser, LoserPoints, IsATie)
-	values (2, 4, 3, 3, False);
+insert into Match (TournamentID, Winner, WinnerPoints,
+	Loser, LoserPoints, IsATie)
+	values (1, 1, 2, 2, 0, False);
+insert into Match (TournamentID, Winner, WinnerPoints,
+	Loser, LoserPoints, IsATie)
+	values (1, 3, 1, 4, 0, False);
+insert into Match (TournamentID, Winner, WinnerPoints,
+	Loser, LoserPoints, IsATie)
+	values (1, 1, 3, 3, 2, False);
+insert into Match (TournamentID, Winner, WinnerPoints,
+	Loser, LoserPoints, IsATie)
+	values (1, 2, 1, 4, 0, False);
+insert into Match (TournamentID, Winner, WinnerPoints,
+	Loser, LoserPoints, IsATie)
+	values (1, 1, 3, 4, 1, False);
+insert into Match (TournamentID, Winner, WinnerPoints,
+	Loser, LoserPoints, IsATie)
+	values (1, 2, 4, 3, 3, False);
 
 -- report fencing matches
-insert into Match (Winner, WinnerPoints, Loser, LoserPoints, IsATie)
-	values (5, 12, 6, 3, False);
-insert into Match (Winner, WinnerPoints, Loser, LoserPoints, IsATie)
-	values (7, 9, 8, 4, False);
-insert into Match (Winner, WinnerPoints, Loser, LoserPoints, IsATie)
-	values (5, 11, 7, 10, False);
-insert into Match (Winner, WinnerPoints, Loser, LoserPoints, IsATie)
-	values (6, 8, 8, 2, False);
-insert into Match (Winner, WinnerPoints, Loser, LoserPoints, IsATie)
-	values (5, 14, 8, 12, False);
-insert into Match (Winner, WinnerPoints, Loser, LoserPoints, IsATie)
-	values (6, 11, 7, 7, False);
+insert into Match (TournamentID, Winner, WinnerPoints,
+	Loser, LoserPoints, IsATie)
+	values (2, 5, 12, 6, 3, False);
+insert into Match (TournamentID, Winner, WinnerPoints,
+	Loser, LoserPoints, IsATie)
+	values (2, 7, 9, 8, 4, False);
+insert into Match (TournamentID, Winner, WinnerPoints,
+	Loser, LoserPoints, IsATie)
+	values (2, 5, 11, 7, 10, False);
+insert into Match (TournamentID, Winner, WinnerPoints,
+	Loser, LoserPoints, IsATie)
+	values (2, 6, 8, 8, 2, False);
+insert into Match (TournamentID, Winner, WinnerPoints,
+	Loser, LoserPoints, IsATie)
+	values (2, 5, 14, 8, 12, False);
+insert into Match (TournamentID, Winner, WinnerPoints,
+	Loser, LoserPoints, IsATie)
+	values (2, 6, 11, 7, 7, False);
